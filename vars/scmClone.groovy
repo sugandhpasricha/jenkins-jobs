df66c04c873f383eadd7b31be01f7d4f05adeb47
sugandhpasricha <sugandh.pasricha16@gmail.com>
2022-05-09 10:57:08 +0530

    updated scm lib

--- vars/scmClone.groovy
+++ vars/scmClone.groovy
@@ -1,5 +1,5 @@
 #!/usr/bin/env groovy
 
-def call(String git_branch = 'human', git_url = 'url') {
-  git branch: '${git_branch}', credentialsId: '2c8ee9e0-6907-4525-88f7-1ebc505f89a0', url: '${git_url}'
+def call(Map config = [:]){
+  git branch: '${config.git_branch}', credentialsId: '2c8ee9e0-6907-4525-88f7-1ebc505f89a0', url: '${config.git_url}'
 }
